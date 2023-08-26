69cfdab1a7823374879177ccd1d7d88e62c4ea74
KidoMARU09 <breinerasencio16@gmail.com>
2023-08-25 22:12:55 -0500

    Se realizaron los ejercicios 3, 4, 5, 7 y 8 del taller con el mismo metodo que en Java

---
+++ DesigualdaTriangularEjercicio7.py
@@ -0,0 +1,19 @@
+def main():
+
+
+    a = float(input("Ingrese el lado a: "))
+    b = float(input("Ingrese el lado b: "))
+    c = float(input("Ingrese el lado c: "))
+
+    if a + b > c and a + c > b and b + c > a:
+        if a == b and b == c and c == a:
+            print("El triángulo es equilátero.")
+        elif a == b or a == c or b == c:
+            print("El triángulo es isósceles.")
+        elif a != b and a != c and b != c:
+            print("El triángulo es escaleno.")
+    else:
+        print("Las medidas ingresadas no cumplen la desigualdad triangular.")
+
+if __name__ == "__main__":
+    main()
